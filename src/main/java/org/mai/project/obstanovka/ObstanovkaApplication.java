b2015dc7ac160c8351d9a4a716e6f8d4b4dc28aa
Влад-Беляков <vladbelyakovv@gmail.com>
2022-11-07 21:14:16 +0300

    starting project

---
+++ src/main/java/org/mai/project/obstanovka/ObstanovkaApplication.java
@@ -0,0 +1,13 @@
+package org.mai.project.obstanovka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObstanovkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObstanovkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mai/project/obstanovka/ObstanovkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mai.project.obstanovka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObstanovkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

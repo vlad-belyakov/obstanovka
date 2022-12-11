c153c424ad5c563735a10e1bf0238bc7db47226b
Влад-Беляков <vladbelyakovv@gmail.com>
2022-12-11 21:18:15 +0300

    конвертация данных - FilesTool

--- src/main/java/org/mai/project/obstanovka/Develop.java
+++ src/main/java/org/mai/project/obstanovka/Develop.java
@@ -1,5 +1,7 @@
 package org.mai.project.obstanovka;
 
 public class Develop {
-
+    public static void main(String[] args) {
+        new FilesTool();
+    }
 }
--- src/main/java/org/mai/project/obstanovka/FilesTool.java
+++ src/main/java/org/mai/project/obstanovka/FilesTool.java
@@ -1,6 +1,7 @@
 package org.mai.project.obstanovka;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,12 +12,39 @@ public class FilesTool {
     private byte[] byteData;
     private String[] stringData;
 
+    public static void main(String[] args) {
+        new FilesTool();
+    }
+
     public FilesTool(String strPath) {
         this.strPath = strPath;
         path = Paths.get(this.strPath);
         if (!Files.exists(path))
             System.out.println("файл " + strPath + " не был найден");
         else readByteData(path);
+    }
+
+    public FilesTool(){
+        byteData = ("1a cf fc 1d 3c 88 0f d7 0c 00 00 00 08 17 04 71\n"/* +
+                        "00 30 20 00 20 00 00 1a cf fc 1d 0c 88 43 c0 0f\n" +
+                        "00 01 00 08 17 04 71 20 00 00 30 20 01 72 6d 3a\n" +
+                        "20 1a cf fc 1d 0c 88 44 c0 26 00 01 00 08 17 04\n" +
+                        "71 20 00 00 30 20 01 63 61 6e 6e 6f 74 20 72 65\n" +
+                        "6d 6f 76 65 20 60 2f 72 6f 6f 74 2f 2a 2e 64 61\n" +
+                        "74 27 1a cf fc 1d 0c 88 45 c0 26 00 01 00 08 17\n" +
+                        "04 71 20 00 00 30 20 01 3a 20 4e 6f 20 73 75 63\n" +
+                        "68 20 66 69 6c 65 20 6f 72 20 64 69 72 65 63 74\n" +
+                        "6f 72 79 1a cf fc 1d 0c 88 46 c0 0c 00 01 00 08\n" +
+                        "17 04 71 20 00 00 30 20 01 0a 1a cf fc 1d 9c 88\n" +
+                        "1f c6 0d 01 01 00 08 17 04 71 20 00 00 30 00 02\n" +
+                        "35 4c a1 36 d4 2c 37 4c a1 36 da 2c 37 4c 8d 36\n" +
+                        "e4 2c 35 4c a3 36 d5 2c 35 4c 94 36 db 2c 35 4c\n" +
+                        "9b 36 e1 2c 31 4c 93 36 eb 2c 35 4c 97 36 e2 2c\n" +
+                        "34 4c 95 36 d9 2c 3f 4c 9a 36 db 2c 3a 4c 97 36"*/).getBytes(StandardCharsets.UTF_8);
+        stringData = obstanovkaToStrData();
+        for (int i = 0; i <= stringData.length - 1; i++) {
+            System.out.println(stringData[i]);
+        }
     }
 
     private void readByteData(Path path) {
@@ -75,28 +103,32 @@ public class FilesTool {
                 j++;
             }
         }
+
         return convertedData;
     }
 
     private String[] obstanovkaToStrData(){
         String[] convertedData;
-        boolean two = false;
+        boolean two = true;
+        int j = 0;
         if (byteData.length % 2 == 0) {
             convertedData = new String[byteData.length / 2];
         } else {
             convertedData = new String[(byteData.length + 1) / 2];
-            two = true;
+            two = false;
         }
         if(two) {
             for (int i = 0; i <= byteData.length - 1; i = i + 2) {
-                convertedData[i] = byteToStr(byteData[i]) + byteToStr(byteData[i + 1]);
+                convertedData[j] = byteToStr(byteData[i]) + byteToStr(byteData[i + 1]);
+                j++;
             }
         }else{
             for (int i = 0; i <= byteData.length - 1; i = i + 2) {
-                if(i != byteData.length - 1)
-                convertedData[i] = byteToStr(byteData[i]) + byteToStr(byteData[i + 1]);
-                else {
-                    convertedData[i] = byteToStr(byteData[i]) + "нечетное кол-во байт в файле";
+                if(i != byteData.length - 1) {
+                    convertedData[j] = byteToStr(byteData[i]) + byteToStr(byteData[i + 1]);
+                    j++;
+                } else {
+                    convertedData[j] = byteToStr(byteData[i]) + "нечетное кол-во байт в файле";
                 }
             }
         }
